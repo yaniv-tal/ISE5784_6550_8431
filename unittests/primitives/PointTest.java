505e6d619b00d7725ba6ae3db59125546c4eedb7
yaniv-tal <tal732349@gmail.com>
2024-01-02 17:40:20 +0200

    PRO1

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,36 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for primitives.Point class
+ * @author Yaniv Tal and Ahuvya Betzalel
+ */
+class PointTest {
+    /**
+     * Test method for {@link primitives.Point#subtract(primitives.Point)}.
+     */
+    @Test
+    void testSubtract() {
+    }
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Vector)}.
+     */
+    @Test
+    void testAdd() {
+    }
+    /**
+     * Test method for {@link primitives.Point#distanceSquared(primitives.Point)}.
+     */
+    @Test
+    void testDistanceSquared() {
+    }
+    /**
+     * Test method for {@link primitives.Point#distance(primitives.Point)}.
+     */
+    @Test
+    void testDistance() {
+    }
+}
---
+++ unittests/primitives/VectorTest.java
@@ -0,0 +1,58 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Unit tests for primitives.Vector class
+ * @author Yaniv Tal and Ahuvya Betzalel
+ */
+class VectorTest {
+    Vector v1         = new Vector(1, 2, 3);
+    Vector v1Opposite = new Vector(-1, -2, -3);
+    Vector v2         = new Vector(-2, -4, -6);
+    Vector v3         = new Vector(0, 3, -2);
+    Vector v4         = new Vector(1, 2, 2);
+    /**
+     * Test method for {@link primitives.Vector#add(primitives.Vector)}.
+     */
+    @Test
+    void testAdd() {
+    }
+    /**
+     * Test method for {@link primitives.Vector#scale(double)}.
+     */
+    @Test
+    void testScale() {
+    }
+    /**
+     * Test method for {@link primitives.Vector#dotProduct(primitives.Vector)}.
+     */
+    @Test
+    void testDotProduct() {
+    }
+    /**
+     * Test method for {@link primitives.Vector#crossProduct(primitives.Vector)}.
+     */
+    @Test
+    void testCrossProduct() {
+    }
+    /**
+     * Test method for {@link primitives.Vector#lengthSquared()}.
+     */
+    @Test
+    void testLengthSquared() {
+    }
+    /**
+     * Test method for {@link primitives.Vector#length()}.
+     */
+    @Test
+    void testLength() {
+    }
+    /**
+     * Test method for {@link primitives.Vector#normalize()}.
+     */
+    @Test
+    void testNormalize() {
+    }
+}
