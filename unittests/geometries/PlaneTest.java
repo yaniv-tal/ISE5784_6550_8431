3e72b76cbf6e26c4aa3d3cf7fabbbcca2d3d1496
yaniv-tal <tal732349@gmail.com>
2024-01-06 21:37:01 +0200

    PRO1

--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -1,6 +1,7 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Double3;
 import primitives.Point;
 import primitives.Vector;
 
@@ -10,15 +11,19 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Yaniv Tal and Ahuvya Betzalel
  */
 class PlaneTest {
+    /**
+     * Test method for {@link Plane#Plane(Point, Point, Point)} and for {@link Plane#Plane(Point,Vector)}
+     */
     @Test
     void testConstructor(){
         // =============== Boundary Values Tests ==================
+        // TC01: The first and second points are same.
         assertThrows(IllegalArgumentException.class,
-                () -> new Plane(new Point(1, 0 ,0),new Point (1,0,0), new Point (1,3,3)),
+                () -> new Plane(new Point(1, 0 ,0),new Point (1,0,0), new Point (1,7,9)),
                 "ERROR: constructor does not throw error in illegal definition- 2 correlating points.");
-
+        // TC02: The points are on the same line.
         assertThrows(IllegalArgumentException.class,
-                () -> new Plane(new Point(1, 1 ,1),new Point (2,2,2), new Point (0,0,0)),
+                () -> new Plane(new Point(1, 1 ,1),new Point (2,2,2), new Point (3,3,3)),
                 "ERROR: constructor does not throw error in illegal definition - all points on same line.");
     }
 
@@ -28,6 +33,7 @@ class PlaneTest {
     @Test
     void GetNormal() {
         // =============== Boundary Values Tests ==================
+        // TC01: Test the normal to the plane.
         Plane plane = new Plane(new Point(0,0,0),new Point(0,1,0),new Point(1,0,0));
         assertEquals(new Vector(0,0,1),plane.getNormal(),"ERROR: normal to plane does not work correctly");
         assertEquals(1, plane.getNormal().length(), "ERROR: plane normalized function does not work correctly");
