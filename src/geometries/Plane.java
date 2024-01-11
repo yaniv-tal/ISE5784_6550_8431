ddb547fed34781ed382dc3d61752b8baec79562e
yaniv-tal <tal732349@gmail.com>
2024-01-11 16:58:59 +0200

    PRO2

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -6,17 +6,20 @@ import primitives.Vector;
 
 import java.util.List;
 
+import static primitives.Util.isZero;
+
 /**
  * the class implements a plane.
  * @author Yaniv and Ahuvya.
  */
-public class Plane implements Geometry{
+public class Plane implements Geometry {
     private final Point q;
     private final Vector normal;
 
     /**
      * constructor. Gets a point and a vector,
      * normalizes the vector and creates the surface.
+     *
      * @param q
      * @param normal
      */
@@ -27,11 +30,12 @@ public class Plane implements Geometry{
 
     /**
      * constructor. Gets 3 point, and creates the surface.
+     *
      * @param x
      * @param y
      * @param z
      */
-    public Plane(Point x, Point y, Point z){
+    public Plane(Point x, Point y, Point z) {
         this.q = x;
         this.normal = z.subtract(y).crossProduct(z.subtract(x)).normalize();
     }
@@ -50,6 +54,23 @@ public class Plane implements Geometry{
 
     @Override
     public List<Point> findIntersections(Ray ray) {
-        return null;
+        Point head = ray.getHead();
+        Vector direction = ray.getDirection();
+
+        // Check for zero vector.
+        if (head.equals(q))
+            return null;
+
+        // Check that the denominator is not zero.
+        double nv = normal.dotProduct(direction);
+        if (isZero(nv))
+            return null;
+
+        double t = normal.dotProduct(q.subtract(head)) / nv;
+
+        //check that the calculate is positive.
+        if (t <= 0)
+            return null;
+        return List.of(ray.getPoint(t));
     }
 }
