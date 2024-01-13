f29fa9fa340786d10af1f17a20bf80e1e87c968f
yaniv-tal <tal732349@gmail.com>
2024-01-13 23:09:23 +0200

    PRO2

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -15,7 +15,6 @@ public class Triangle extends Polygon {
      * constructor. Gets an array of vertices,
      * calls the polygon constructor
      * and creates the triangle.
-     *
      * @param vertices
      */
     public Triangle(Point... vertices) {
