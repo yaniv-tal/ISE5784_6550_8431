c2670de3f03e6e4b6abdaebaf4c6eb1acf960ee1
yaniv-tal <tal732349@gmail.com>
2024-01-16 17:24:18 +0200

    PRO2

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -3,6 +3,7 @@ package renderer;
 import primitives.*;
 
 import java.util.MissingResourceException;
+import java.util.prefs.PreferenceChangeEvent;
 
 import static primitives.Util.isZero;
 
@@ -17,20 +18,25 @@ public class Camera implements Cloneable {
 
     /**
      * get function for the width of the View Plane.
+     *
      * @return the width of the View plane.
      */
     public double getWidth() {
         return width;
     }
+
     /**
      * get function for the height of the View Plane.
+     *
      * @return the height of the View plane.
      */
     public double getHeight() {
         return height;
     }
+
     /**
      * get function for the distance of the View Plane.
+     *
      * @return the distance of the View plane.
      */
     public double getDistance() {
@@ -40,26 +46,52 @@ public class Camera implements Cloneable {
     /**
      * Default constructor.
      */
-    private Camera(){}
+    private Camera() {
+    }
 
     /**
      * function for a new Builder object.
+     *
      * @return a new Builder object.
      */
-    public static Builder getBuilder() { return new Builder(); }
+    public static Builder getBuilder() {
+        return new Builder();
+    }
 
-    /** להשלים
+    /**
+     * להשלים
      *
      * @param nX - Represents the amount of columns (row width)
      * @param nY - represents the number of rows (column height).
-     * @param j - column number
-     * @param i - row number
+     * @param j  - column number
+     * @param i  - row number
      * @return
      */
-    public Ray constructRay(int nX, int nY, int j, int i){ return null;};
+    public Ray constructRay(int nX, int nY, int j, int i) {
+        Point pCenter = p0.add(vTo.scale(distance));
+
+        //Ratio
+        double Ry = (double) height / nY;
+        double Rx = (double) width / nX;
+
+        //Pixel[i,j] center
+        double yI = -(i - (double) (nY - 1) / 2) * Ry;
+        double xJ = (j - (double) (nX - 1) / 2) * Rx;
+        Point pIJ = pCenter;
+        if (xJ != 0)
+            pIJ = pIJ.add(vRight.scale(xJ));
+        if (yI != 0)
+            pIJ = pIJ.add(vUp.scale(yI));
+
+        Vector vIJ = pIJ.subtract(p0);
+        return new Ray(p0, vIJ);
+    }
+
+    ;
 
     /**
      * The class implements a Builder Design Pattern.
+     *
      * @author Yaniv and Ahuvya.
      */
     public static class Builder {
@@ -67,6 +99,7 @@ public class Camera implements Cloneable {
 
         /**
          * Determining the location of the camera
+         *
          * @param location
          * @return Returns the Builder object
          */
@@ -77,13 +110,14 @@ public class Camera implements Cloneable {
 
         /**
          * Determining the Direction of the camera
+         *
          * @param to vector
          * @param up vector
          * @return Returns the Builder object
          */
         public Builder setDirection(Vector to, Vector up) {
             //Correctness check, make sure they are orthogonal.
-            if( !Util.isZero((to.dotProduct(up))))
+            if (!Util.isZero((to.dotProduct(up))))
                 throw new IllegalArgumentException("vTo and vUp must be orthogonal");
             camera.vTo = to.normalize();
             camera.vUp = up.normalize();
@@ -92,6 +126,7 @@ public class Camera implements Cloneable {
 
         /**
          * set the size of the view plane
+         *
          * @param width
          * @param height
          * @return Returns the Builder object
@@ -107,37 +142,51 @@ public class Camera implements Cloneable {
 
         /**
          * to set the distance between camera and view plane
+         *
          * @param distance
          * @return Returns the Builder object
          */
         public Builder setVpDistance(double distance) {
             //Correctness check, make sure they are positive.
-            if (Util.alignZero(camera.distance) <= 0) throw new IllegalArgumentException("The distance should be bigger than 0");
+            if (Util.alignZero(distance) <= 0)
+                throw new IllegalArgumentException("The distance should be bigger than 0");
             camera.distance = distance;
             return this;
         }
 
         public Camera build() {
             //Correctness check, make sure that the vectors and the points are not "NULL".
-            if (camera.p0 == null) throw new MissingResourceException("Missing rendering argument", "Camera","p0");
-            if (camera.vUp == null) throw new MissingResourceException("Missing rendering argument", "Camera","vUp");
-            if (camera.vTo == null) throw new MissingResourceException("Missing rendering argument", "Camera","vTo");
+            if (camera.p0 == null) throw new MissingResourceException("Missing rendering argument", "Camera", "p0");
+            if (camera.vUp == null) throw new MissingResourceException("Missing rendering argument", "Camera", "vUp");
+            if (camera.vTo == null) throw new MissingResourceException("Missing rendering argument", "Camera", "vTo");
             //Correctness check, make sure that the values are not 0.
-            if (Util.alignZero(camera.width) == 0) throw new MissingResourceException("Missing rendering argument", "Camera","width");
-            if (Util.alignZero(camera.height) == 0) throw new MissingResourceException("Missing rendering argument", "Camera","height");
-            if (Util.alignZero(camera.distance) == 0) throw new MissingResourceException("Missing rendering argument", "Camera","distance");
+            if (Util.alignZero(camera.width) == 0)
+                throw new MissingResourceException("Missing rendering argument", "Camera", "width");
+            if (Util.alignZero(camera.height) == 0)
+                throw new MissingResourceException("Missing rendering argument", "Camera", "height");
+            if (Util.alignZero(camera.distance) == 0)
+                throw new MissingResourceException("Missing rendering argument", "Camera", "distance");
             //Correctness check, make sure that the values are positives.
             if (Util.alignZero(camera.width) < 0) throw new IllegalArgumentException("The width can't be negative");
             if (Util.alignZero(camera.height) < 0) throw new IllegalArgumentException("The height can't be negative");
-            if (Util.alignZero(camera.distance) < 0) throw new IllegalArgumentException("The distance can't be negative");
+            if (Util.alignZero(camera.distance) < 0)
+                throw new IllegalArgumentException("The distance can't be negative");
             //Correctness check, make sure that vTo and vUp are orthogonal.
-            if (!isZero(camera.vUp.dotProduct(camera.vTo))) throw new IllegalArgumentException("vTo and vUp must be orthogonal");
+            if (!isZero(camera.vUp.dotProduct(camera.vTo)))
+                throw new IllegalArgumentException("vTo and vUp must be orthogonal");
             camera.vRight = camera.vTo.crossProduct(camera.vUp).normalize();
             //Correctness check, make sure that vTo, vUp and vRight are normalized.
-            if(!Util.isZero(camera.vTo.lengthSquared()-1)) throw new IllegalArgumentException("The vTo vector must be normalized");
-            if(!Util.isZero(camera.vUp.lengthSquared()-1)) throw new IllegalArgumentException("The vUp vector must be normalized");
-            if(!Util.isZero(camera.vRight.lengthSquared()-1)) throw new IllegalArgumentException("The vRight vector must be normalized");
-            return (Camera) camera.clone();
+            if (!Util.isZero(camera.vTo.lengthSquared() - 1))
+                throw new IllegalArgumentException("The vTo vector must be normalized");
+            if (!Util.isZero(camera.vUp.lengthSquared() - 1))
+                throw new IllegalArgumentException("The vUp vector must be normalized");
+            if (!Util.isZero(camera.vRight.lengthSquared() - 1))
+                throw new IllegalArgumentException("The vRight vector must be normalized");
+            try {
+                return (Camera) camera.clone();
+            } catch (CloneNotSupportedException ignore) {
+                return null;
+            }
         }
     }
 }
---
+++ unittests/renderer/CameraRayIntegrationTest.java
@@ -0,0 +1,73 @@
+package renderer;
+
+import geometries.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
+
+import java.util.List;
+
+public class CameraRayIntegrationTest {
+    private final Camera.Builder cameraBuilder = Camera.getBuilder()
+            .setDirection(new Vector(0, 0, -1), new Vector(0, -1, 0))
+            .setVpSize(3, 3)
+            .setVpDistance(1);
+
+    private int calculateIntersections(Geometry geometry, Camera camera) {
+        int sumOfIntersections = 0;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                var intersections = geometry.findIntersections(camera.constructRay(3, 3, j, i));
+                if (intersections != null)
+                    sumOfIntersections += intersections.size();
+            }
+        return sumOfIntersections;
+    }
+
+    private final Camera camera1 = cameraBuilder.setLocation(Point.ZERO).build();
+    private final Camera camera2 = cameraBuilder.setLocation(new Point(0, 0, 0.5)).build();
+
+    @Test
+    void testSphere() {
+        //TC01: 2 intersections.
+        Sphere sphere = new Sphere(1, new Point(0, 0, -3));
+        assertEquals(2, calculateIntersections(sphere, camera1), "ERROR: Wrong number of intersections.");
+        //TC02: 18 intersections.
+        sphere = new Sphere(2.5, new Point(0, 0, -2.5));
+        assertEquals(18, calculateIntersections(sphere, camera2), "ERROR: Wrong number of intersections.");
+        //TC03: 10 intersections.
+        sphere = new Sphere(2, new Point(0, 0, -2));
+        assertEquals(10, calculateIntersections(sphere, camera2), "ERROR: Wrong number of intersections.");
+        //TC04: 9 intersections.
+        sphere = new Sphere(4, new Point(0, 0, -1.5));
+        assertEquals(9, calculateIntersections(sphere, camera1), "ERROR: Wrong number of intersections.");
+        //TC05: no intersections.
+        sphere = new Sphere(0.5, new Point(0, 0, 1));
+        assertEquals(0, calculateIntersections(sphere, camera1), "ERROR: Wrong number of intersections.");
+    }
+
+    @Test
+    void testPlane() {
+        //TC01: 9 intersections.
+        Plane plane = new Plane(new Point(0, 0, -2.5), new Vector(0, 0, 1));
+        assertEquals(9, calculateIntersections(plane, camera1), "ERROR: Wrong number of intersections.");
+        //TC02: 9 intersections.
+        plane = new Plane(new Point(0, 0, -2), new Vector(0, -1, 3));
+        assertEquals(9, calculateIntersections(plane, camera1), "ERROR: Wrong number of intersections.");
+        //TC03: 6 intersections.
+        plane = new Plane(new Point(0, 0, -2.5), new Vector(0, -1, 1));
+        assertEquals(6, calculateIntersections(plane, camera1), "ERROR: Wrong number of intersections.");
+    }
+
+    @Test
+    void testTriangle() {
+        //TC01: One intersection.
+        Triangle triangle = new Triangle(new Point(0, 1, -2), new Point(1, -1, -2), new Point(-1, -1, -2));
+        assertEquals(1, calculateIntersections(triangle, camera1), "ERROR: Wrong number of intersections.");
+        //TC02: 2 intersections.
+        triangle = new Triangle(new Point(0, 20, -2), new Point(1, -1, -2), new Point(-1, -1, -2));
+        assertEquals(2, calculateIntersections(triangle, camera1), "ERROR: Wrong number of intersections.");
+    }
+}
