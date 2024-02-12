ad71690f45522e19339cf54a9fd6d1f0b3193f8e
ahuvyab <ahuvyab@gmail.com>
2024-02-12 02:48:47 +0200

    tate

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -15,19 +15,15 @@ class ImageWriterTest {
     void imageWriterTest() {
         ImageWriter imageTest = new ImageWriter("imageTest", 800, 500);
         //We will go by the pixels we received and create longitude and latitude lines in black
-        for (int j = 0; j < 800; j+= 50) {
-            if (isZero(j % 50))
-                for (int i = 0; i < 500; i++) {
-                    imageTest.writePixel(j, i, Color.BLACK);
-                }
-            else
-                for (int i = 0; i < 500; i++) {
-                    if (isZero(i % 50))
-                        imageTest.writePixel(j, i, Color.BLACK);
-                    else
-                        imageTest.writePixel(j, i, new Color(64, 228, 204));//turquoise
-                }
-        }
+        for (int i = 0; i < 500; i++)
+            for (int j = 0; j < 800; j++)
+                imageTest.writePixel(j, i, new Color(64, 228, 204));//turquoise
+        for (int j = 0; j < 800; j+=50)
+            for (int i = 0; i < 500; i++)
+                imageTest.writePixel(j, i, Color.BLACK);
+        for (int i = 0; i < 500; i+=50)
+            for (int j = 0; j < 800; j++)
+                imageTest.writePixel(j, i, Color.BLACK);
         imageTest.writeToImage();
     }
 }
